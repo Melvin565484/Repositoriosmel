d9c4a4229e0450c1ebbd3e5fca20204b3f5f490e
Melvin Mendoza <melsmendoza17@gmail.com>
2021-08-24 20:34:59 -0300

    semana2ejercicio1

---
+++ Saludo/Saludo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saludo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Crear un programa en C# que escriba un saludo y muestre su nombre (ej. "Hola, Alberto")
+            string nombre = " "; 
+            Console.WriteLine("¿Como te llamas?"); 
+            Console.WriteLine("--------------------------------------------------------------");
+            nombre = Console.ReadLine(); 
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Hola {0}", nombre); 
+           
+        }
+    }
+}
